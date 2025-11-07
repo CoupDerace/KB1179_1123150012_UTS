bda1ee79d94e91161667b4c9086f133da2a3404f
CoupDerace <turtusibae@gmail.com>
2025-11-07 13:30:39 +0700

    menambahkan icon pada login page

--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -6,7 +6,23 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              const Icon(
+                Icons.qr_code_2,
+                size: 100,
+                color: Colors.deepOrange,
+              ),
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
