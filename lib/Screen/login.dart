8aa129f88c112dc4cc7ab4a6de2e863bd43030f6
CoupDerace <turtusibae@gmail.com>
2025-11-07 13:32:04 +0700

    menambahkan text pada login page

--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -13,12 +13,21 @@ class Login extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(height: 20),
-              const Icon(
-                Icons.qr_code_2,
-                size: 100,
-                color: Colors.deepOrange,
+              const Icon(Icons.qr_code_2, size: 100, color: Colors.deepOrange),
+              const SizedBox(height: 10),
+              const Text(
+                "Selamat Datang di TourTravel",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
               ),
-              
+              const SizedBox(height: 10),
+              const Text(
+                "Silahkan login terlebih dahulu untuk melanjutkan",
+                style: TextStyle(fontSize: 16.0, color: Colors.grey),
+              ),
             ],
           ),
         ),
