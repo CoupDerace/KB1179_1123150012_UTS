5e5ff2fe143133077e9ebc3f538798a4b81d7001
CoupDerace <turtusibae@gmail.com>
2025-11-07 10:58:44 +0700

    menggunakan MaterialApp dan menambahkan theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "Project UTS",
+      theme:  ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+    );
   }
 }
